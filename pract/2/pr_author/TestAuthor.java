9a1786ba624f25aa727d17e365b63b01e96ad981
a274 <56553423+a274@users.noreply.github.com>
2020-09-16 09:23:10 +0300

    Add files via upload

---
+++ pract/2/pr_author/TestAuthor.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        // write your code here
+        Author author = new Author("Ivan Popov", "ivPopov@somewhere.com", 'm');
+        System.out.println(author);
+    }
+}
